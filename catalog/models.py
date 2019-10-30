5a5d4ada526c08b7369352c3967f1b593958dd7a
Bel <bel@Hallucination.local>
2019-10-30 14:52:24 -0500

    structures

--- catalog/models.py
+++ catalog/models.py
@@ -1,23 +1,30 @@
 from django.db import models
 
-# Create your models here.
-class User(models.Model):
-    # Fields
-    user_name = models.CharField(max_length=100, help_text='What is your name?')
-    #email = models.EmailField(default='DEFAULT')
-    #user_public_key = models.CharField(max_length=256, default='DEFAULT')
-
-    # Methods
-    def __str__(self):
-        return self.user_name
-
 class Door(models.Model):
     # Fields
     door_name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     #user_list = models.ManyToManyField(User, help_text="List of Authorized Users")
-    #door_public_key = models.CharField(max_length=256, default='DEFAULT')
+    door_public_key = models.CharField(max_length=256)
 
     # Methods
     def __str__(self):
         return self.door_name
+
+# Create your models here.
+class User(models.Model):
+    # Fields
+    netid = models.CharField(max_length=100, null='True', help_text='What is your NetID?')
+    first_name = models.CharField(max_length=100, default='DEFAULT')
+    last_name = models.CharField(max_length=100, default='DEFAULT')
+    user_public_key = models.CharField(max_length=256, default='DEFAULT')
+    door = models.ManyToManyField(Door, related_name=None)
+
+
+    # Order
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    # Methods
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
