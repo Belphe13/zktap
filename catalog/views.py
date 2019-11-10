16466e8a4179d26edf1937a31b116075961a73b9
Bel <bel@Hallucination.local>
2019-11-09 23:09:20 -0600

    sloppy homepage

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Door, User
 
 # Create your views here.
+def index(request):
+    #num_doors = Door.objects.all().count()
+    return render(request, 'index.html')
