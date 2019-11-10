a35b2fcf02fc674b5cf6cbeaa019694c1862f56e
Bel <bel@Hallucination.local>
2019-11-10 12:22:00 -0600

    homepage

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+#    path('doors/', views.DoorListView.as_view(), name='doors'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from .models import Door, User
+from django.views import generic
 
 # Create your views here.
 def index(request):
-    #num_doors = Door.objects.all().count()
-    return render(request, 'index.html')
+    # Generate counts of some of the main objects
+    num_doors = Door.objects.all().count()
+    num_users = User.objects.all().count()
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
+
+    context = {
+            'num_doors':num_doors, 
+            'num_users':num_users, 
+            'num_visits':num_visits
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context = context)
+
